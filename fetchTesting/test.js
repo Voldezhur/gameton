03bb2d5ebcf7e4103911b5373f1292e2ee39ea64
Voldezhur <voldezhur@gmail.com>
2024-04-05 17:19:08 +0300

    further testing with json

--- fetchTesting/test.js
+++ fetchTesting/test.js
@@ -1,11 +1,27 @@
+const Token = "660a858a24615660a858a24619"
+
 async function fetchUniverse() {
     const response = await fetch("https://datsedenspace.datsteam.dev/player/universe", {
         headers: {
-            "X-Auth-Token": "660a858a24615660a858a24619"
+            "X-Auth-Token": Token
         }
     });
     const res = await response.json();
-    return (res);
+    console.log(res);
 }
 
-fetchUniverse();
+async function fetchShip() {
+    const response = await fetch("https://datsedenspace.datsteam.dev/player/universe", {
+        headers: {
+            "X-Auth-Token": Token
+        }
+    });
+    const res = await response.text();
+    console.log(JSON.parse(res)["ship"]);
+}
+
+async function goTo() {
+    const response = await fetch
+}
+
+fetchShip();
