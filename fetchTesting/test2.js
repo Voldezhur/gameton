47cd7d63535b27a0a84f41a9832306b0759e3bf6
Voldezhur <voldezhur@gmail.com>
2024-04-05 21:37:14 +0300

    testing travel

---
+++ fetchTesting/test2.js
@@ -0,0 +1,52 @@
+const Token = "660a858a24615660a858a24619"
+
+async function getData(url) {
+    const response = await fetch(url, {
+        headers: {
+            "X-Auth-Token": Token
+        }
+    });
+    const res = await response.text();
+    return res;
+}
+
+async function fetchUniverse() {
+    const data = await getData("https://datsedenspace.datsteam.dev/player/universe")
+    console.log(data)
+}
+
+async function fetchShip() {
+    const data = await getData("https://datsedenspace.datsteam.dev/player/universe")
+    console.log(JSON.parse(data)["ship"]);
+}
+
+async function goTo() {
+    const res = await fetch("https://datsedenspace.datsteam.dev/player/universe", {
+        headers: {
+            "X-Auth-Token": Token
+        }
+    });
+
+    const universe = await res.text();
+    const planets = JSON.parse(universe)["universe"];
+    console.log(planets);
+    
+    // Запрос на полет
+    const flightRes = await fetch("https://datsedenspace.datsteam.dev/player/travel", {
+        method: 'POST',
+        headers: {
+            "X-Auth-Token": Token
+        },
+        body: {
+            "planets": [
+                "Daniel"
+            ]
+        }
+    });
+
+    const flightResponse = await flightRes.text();
+    console.log(flightResponse);
+}
+
+goTo();
+fetchShip();
